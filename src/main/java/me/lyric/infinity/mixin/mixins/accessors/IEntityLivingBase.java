27b993c47c4aacea4329508b40fb03144d5f2eef
lyricnight <79311706+lyricnight@users.noreply.github.com>
2023-04-26 15:10:07 +0100

    instantswap crashes due to access error lol

---
+++ src/main/java/me/lyric/infinity/mixin/mixins/accessors/IEntityLivingBase.java
@@ -0,0 +1,11 @@
+package me.lyric.infinity.mixin.mixins.accessors;
+
+import net.minecraft.entity.EntityLivingBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(value={EntityLivingBase.class})
+public interface IEntityLivingBase {
+    @Invoker(value="getArmSwingAnimationEnd")
+    int getArmSwingAnimationEnd();
+}
