1c585dbdaf790c5ad2a84ddedd5f0402f07ba070
lyricnight <79311706+lyricnight@users.noreply.github.com>
2023-05-09 19:44:55 +0100

    nametag thin

--- src/main/java/me/lyric/infinity/impl/modules/render/NameTags.java
+++ src/main/java/me/lyric/infinity/impl/modules/render/NameTags.java
@@ -1,13 +1,16 @@
 package me.lyric.infinity.impl.modules.render;
 
+import event.bus.EventListener;
 import me.lyric.infinity.Infinity;
+import me.lyric.infinity.api.event.events.render.RenderNametagEvent;
 import me.lyric.infinity.api.module.Category;
 import me.lyric.infinity.api.module.Module;
 import me.lyric.infinity.api.setting.Setting;
-import me.lyric.infinity.api.setting.settings.ColorPicker;
 import me.lyric.infinity.api.util.gl.RenderUtils;
 import me.lyric.infinity.api.util.minecraft.EntityUtil;
 import me.lyric.infinity.mixin.mixins.accessors.IRenderManager;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.culling.ICamera;
 import net.minecraft.enchantment.Enchantment;
@@ -18,34 +21,32 @@ import net.minecraft.item.*;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.text.TextFormatting;
-
-import java.awt.*;
+import org.lwjgl.opengl.GL11;
 import java.util.ArrayList;
 import java.util.Objects;
 
+
 public class NameTags extends Module {
-    public Setting<ColorPicker> colour = register(new Setting<>("Colour", "Colour of border.", new ColorPicker(Color.BLACK)));
-    public Setting<Boolean> armor = register(new Setting<>("Armor","Renders armor.", true));
-    public Setting<Boolean> health = register(new Setting<>("Health","Renders health.", true));
-    public Setting<Boolean> ping = register(new Setting<>("Ping","Renders ping.", true));
-    public Setting<Boolean> gamemode = register(new Setting<>("Gamemode","Renders gamemode.", true));
-    public Setting<Boolean> invisibles = register(new Setting<>("Invisibles","Renders invis entities.", true));
-    public Setting<Boolean> durability = register(new Setting<>("Durability","Renders item dura,",true));
-    public Setting<Boolean> itemName = register(new Setting<>("ItemName","Renders item names.", true));
-    public Setting<Boolean> totemPops = register(new Setting<>("TotemPops","Renders totem pop.",true));
-    public Setting<Boolean> shortEnchants = register(new Setting<>("Enchants","Renders enchantment names.", true));
-    public Setting<Double> scaling = register(new Setting<>("Scaling","scale.", 3.0d, 1.0d, 5.0d));
-    public Setting<Boolean> background = register(new Setting<>("Background","Waste of a setting.", true));
-    public Setting<Boolean> border = register(new Setting<>("Border","if you use this you need help", true));
-    public Setting<Double> borderWidth = register(new Setting<>("BorderWidth","width of border", 1.0d, 0.1d, 3.0d));
+    public Setting<Boolean> armor = register(new Setting<>("Armor","Renders armor overlay.", true));
+    public Setting<Boolean> health = register(new Setting<>("Health","Displays player health.", true));
+    public Setting<Boolean> ping = register(new Setting<>("Ping","Displays ping.", true));
+    public Setting<Boolean> gamemode = register(new Setting<>("Gamemode","Displays gamemode.", true));
+    public Setting<Boolean> invisibles = register(new Setting<>("Invisibles","Reveals invis entities.", true));
+    public Setting<Boolean> durability = register(new Setting<>("Durability","Whether to show item/armor dura.", true));
+    public Setting<Boolean> itemName = register(new Setting<>("ItemName","Shows item names.", true));
+    public Setting<Boolean> totemPops = register(new Setting<>("TotemPops","Displays TotemPops on nametags.", true));
+    public Setting<Boolean> shortEnchants = register(new Setting<>("Enchants","Displays Enchantment names.", true));
+    public Setting<Double> scaling = register(new Setting<>("Scaling","Scale for the nametags.", 3.0d, 1.0d, 5.0d));
+    public Setting<Boolean> border = register(new Setting<>("Border", "Dummy", true));
 
     public static NameTags INSTANCE;
     public NameTags() {
-        super("NameTags","Nametags rendering.", Category.RENDER);
+        super("NameTags","Good Nametags.", Category.RENDER);
         INSTANCE = this;
     }
 
-    private ICamera camera = new Frustum();
+    private final ICamera camera = new Frustum();
+    private float borderWidth = 0.1f;
 
     @Override
     public void onRender3D(float partialTicks) {
@@ -75,18 +76,41 @@ public class NameTags extends Module {
         double tempY = y;
         tempY += player.isSneaking() ? 0.5D : 0.7D;
         Entity camera = mc.getRenderViewEntity();
+
         double originalPositionX = camera.posX;
         double originalPositionY = camera.posY;
         double originalPositionZ = camera.posZ;
+
         camera.posX = interpolate(camera.prevPosX, camera.posX, delta);
         camera.posY = interpolate(camera.prevPosY, camera.posY, delta);
         camera.posZ = interpolate(camera.prevPosZ, camera.posZ, delta);
+
         double distance = camera.getDistance(x + (mc.getRenderManager()).viewerPosX, y + (mc.getRenderManager()).viewerPosY, z +  (mc.getRenderManager()).viewerPosZ);
         int width = (int) (mc.fontRenderer.getStringWidth(getDisplayName(player)) / 2);
         double scale = distance > 8.0d ? 0.0018d + (scaling.getValue() * 0.001d) * distance : 0.0018d + (scaling.getValue() * 0.001d) * 8.0d;
-        RenderUtils.drawBorderedRect(-width - 2.0f, -(mc.fontRenderer.FONT_HEIGHT + 2.3f), width + 4.0f, 1.5F, borderWidth.getValue().floatValue(), colour.getValue().getColor().getRGB(), colour.getValue().getColor().getRGB());
-        mc.fontRenderer.drawStringWithShadow(getDisplayName(player), -width, (int) -(mc.fontRenderer.FONT_HEIGHT + 2), getDisplayColour(player));
+
+        GlStateManager.pushMatrix();
+        RenderHelper.enableStandardItemLighting();
+        GlStateManager.enablePolygonOffset();
+        GlStateManager.doPolygonOffset(1.0F, -1500000.0F);
+        GlStateManager.disableLighting();
+        GlStateManager.translate((float)x, (float)tempY + 1.4F, (float)z);
+        GlStateManager.rotate(-(mc.getRenderManager()).playerViewY, 0.0F, 1.0F, 0.0F);
+        GlStateManager.rotate((mc.getRenderManager()).playerViewX, (mc.gameSettings.thirdPersonView == 2) ? -1.0F : 1.0F, 0.0F, 0.0F);
+        GlStateManager.scale(-scale, -scale, scale);
+        GlStateManager.disableDepth();
+        GlStateManager.depthMask(false);
+        GL11.glDisable(2929);
+        GlStateManager.enableBlend();
+        drawBorderedRect(-width - 2.0f, -(mc.fontRenderer.FONT_HEIGHT + 2.3f), width + 4.0f, 1.5F, borderWidth, 1996488704, 1996488704);
+        GlStateManager.disableBlend();
+        GlStateManager.disableAlpha();
+        GlStateManager.glNormal3f(0.0F, 1.0F, 0.0F);
+        GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+        mc.fontRenderer.drawStringWithShadow(getDisplayName(player), -width, (int) -(mc.fontRenderer.FONT_HEIGHT - 1), getDisplayColour(player));
+        GlStateManager.glNormal3f(0.0F, 0.0F, 0.0F);
         if (armor.getValue()) {
+            GlStateManager.pushMatrix();
             int xOffset = 0;
             if((player.getHeldItemMainhand().getItem() != null && player.getHeldItemOffhand().getItem() == null) || (player.getHeldItemMainhand().getItem() == null && player.getHeldItemOffhand().getItem() != null)) {
                 xOffset = -4;
@@ -148,19 +172,31 @@ public class NameTags extends Module {
                     if (itemName.getValue() && !renderStack.isEmpty() && renderStack.getItem() != Items.AIR) {
                         String stackName = renderStack.getDisplayName();
                         int stackNameWidth = (int) (mc.fontRenderer.getStringWidth(stackName) / 2);
+                        GL11.glPushMatrix();
+                        GL11.glScalef((float)0.5f, (float)0.5f, (float)0.0f);
                         int enchantY = 2-getEnchantSpace(stacks)-24;
                         if(!(enchantY < -50)) {
                             enchantY = -50;
                         }
-                        mc.fontRenderer.drawStringWithShadow(stackName, -stackNameWidth, (int) (enchantY - 20), -1);
+                        mc.fontRenderer.drawString(stackName, -stackNameWidth, (int) (enchantY - 20), -1);
+                        GL11.glScalef((float)1.5f, (float)1.5f, (float)1.0f);
+                        GL11.glPopMatrix();
                     }
                     xOffset += 16;
                 }
             }
+            GlStateManager.popMatrix();
         }
         camera.posX = originalPositionX;
         camera.posY = originalPositionY;
         camera.posZ = originalPositionZ;
+        GlStateManager.enableDepth();
+        GlStateManager.enableLighting();
+        GlStateManager.disableBlend();
+        GlStateManager.enableLighting();
+        GlStateManager.disablePolygonOffset();
+        GlStateManager.doPolygonOffset(1.0F, 1500000.0F);
+        GlStateManager.popMatrix();
     }
 
 
@@ -170,6 +206,52 @@ public class NameTags extends Module {
         }
         return 0;
     }
+    public void drawBorderedRect(float x, float y, float x1, float y1, float width, int internalColor, int borderColor) {
+        float alpha = (borderColor >> 24 & 255) / 255.0F;
+        float red = (borderColor >> 16 & 255) / 255.0F;
+        float green = (borderColor >> 8 & 255) / 255.0F;
+        float blue = (borderColor & 255) / 255.0F;
+        GlStateManager.pushMatrix();
+        enableGL2D();
+        if(border.getValue()) {
+            GL11.glColor4f(red, green, blue, alpha);
+            GL11.glEnable(GL11.GL_BLEND);
+            GL11.glDisable(GL11.GL_TEXTURE_2D);
+            GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+            GL11.glLineWidth(width);
+            GL11.glBegin(GL11.GL_LINE_STRIP);
+            GL11.glVertex2f(x, y);
+            GL11.glVertex2f(x, y1);
+            GL11.glVertex2f(x1, y1);
+            GL11.glVertex2f(x1, y);
+            GL11.glVertex2f(x, y);
+            GL11.glEnd();
+            GL11.glEnable(GL11.GL_TEXTURE_2D);
+            GL11.glDisable(GL11.GL_BLEND);
+        }
+        disableGL2D();
+        GlStateManager.popMatrix();
+    }
+
+    public static void enableGL2D() {
+        GL11.glDisable(2929);
+        GL11.glEnable(3042);
+        GL11.glDisable(3553);
+        GL11.glBlendFunc(770, 771);
+        GL11.glDepthMask(true);
+        GL11.glEnable(2848);
+        GL11.glHint(3154, 4354);
+        GL11.glHint(3155, 4354);
+    }
+
+    public static void disableGL2D() {
+        GL11.glEnable(3553);
+        GL11.glDisable(3042);
+        GL11.glDisable(2848);
+        GL11.glHint(3154, 4352);
+        GL11.glHint(3155, 4352);
+    }
+
     public int getEnchantSpace(ArrayList<ItemStack> items) {
         int biggestEncCount = 0;
         for(ItemStack i : items) {
@@ -212,11 +294,41 @@ public class NameTags extends Module {
         if(!(enchantY < -50)) {
             enchantY = -50;
         }
+
+        GlStateManager.pushMatrix();
+        GlStateManager.depthMask(true);
+        GlStateManager.clear(256);
+        RenderHelper.enableStandardItemLighting();
         (mc.getRenderItem()).zLevel = -150.0F;
+        GlStateManager.disableLighting();
+        GlStateManager.disableDepth();
+        GlStateManager.disableBlend();
+        GlStateManager.enableLighting();
+        GlStateManager.enableDepth();
+        GlStateManager.disableLighting();
+        GlStateManager.disableDepth();
+        GlStateManager.disableAlpha();
+        GlStateManager.disableAlpha();
+        GlStateManager.disableBlend();
+        GlStateManager.enableBlend();
+        GlStateManager.enableAlpha();
+        GlStateManager.enableAlpha();
+        GlStateManager.enableLighting();
+        GlStateManager.enableDepth();
         mc.getRenderItem().renderItemAndEffectIntoGUI(stack, x, armorY);
         mc.getRenderItem().renderItemOverlays(mc.fontRenderer, stack, x, armorY);
         (mc.getRenderItem()).zLevel = 0.0F;
+        RenderHelper.disableStandardItemLighting();
+        GlStateManager.disableCull();
+        GlStateManager.enableAlpha();
+        GlStateManager.disableBlend();
+        GlStateManager.disableLighting();
+        GlStateManager.scale(0.5F, 0.5F, 0.5F);
+        GlStateManager.disableDepth();
         renderEnchantmentText(stack, x, enchantY);
+        GlStateManager.enableDepth();
+        GlStateManager.scale(2.0F, 2.0F, 2.0F);
+        GlStateManager.popMatrix();
     }
 
     private void renderEnchantmentText(ItemStack stack, int x, int y) {
@@ -303,7 +415,7 @@ public class NameTags extends Module {
         }
 
         if (totemPops.getValue() && player.getName() != null && getTotemPops(player.getName()) > 0)
-            name += String.format(" %s-%d", TextFormatting.AQUA, getTotemPops(player.getName()));
+            name += String.format(" %s-%d", TextFormatting.RED, getTotemPops(player.getName()));
 
         return name;
     }
@@ -320,6 +432,11 @@ public class NameTags extends Module {
             colour = -6481515;
 
         return colour;
+    }
+    @EventListener
+    public void onRenderNametag(RenderNametagEvent event) {
+        if (!nullSafe()) return;
+        event.cancel();
     }
 
 
