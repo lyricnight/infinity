b9c5426af5fd5c9f8cbeabde0fc6debb7b9f18b5
lyricnight <79311706+lyricnight@users.noreply.github.com>
2023-05-29 13:36:52 +0100

    fixed newbase compat

--- src/main/java/me/lyric/infinity/mixin/mixins/entity/MixinEntityPlayerSP.java
+++ src/main/java/me/lyric/infinity/mixin/mixins/entity/MixinEntityPlayerSP.java
@@ -15,7 +15,6 @@ import net.minecraft.network.play.client.CPacketAnimation;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -25,9 +24,7 @@ import java.util.Objects;
 
 @Mixin(EntityPlayerSP.class)
 public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
-
-    @Shadow protected abstract void onUpdateWalkingPlayer();
-    private static Minecraft mc = Minecraft.getMinecraft();
+    private Minecraft mc = Minecraft.getMinecraft();
     private UpdateWalkingPlayerEventPre eventUpdateWalkingPlayer;
 
     public MixinEntityPlayerSP(World worldIn, GameProfile playerProfile) {
