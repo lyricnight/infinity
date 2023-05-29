e94ff6cbcb306526739e324a9902117a04532a79
lyricnight <79311706+lyricnight@users.noreply.github.com>
2023-05-29 15:59:40 +0100

    added colorchanger support to timestamps

--- src/main/java/me/lyric/infinity/impl/modules/misc/BetterChat.java
+++ src/main/java/me/lyric/infinity/impl/modules/misc/BetterChat.java
@@ -1,11 +1,11 @@
 package me.lyric.infinity.impl.modules.misc;
 
-import com.mojang.realmsclient.gui.ChatFormatting;
 import me.bush.eventbus.annotation.EventListener;
 import me.lyric.infinity.api.event.network.PacketEvent;
 import me.lyric.infinity.api.module.Category;
 import me.lyric.infinity.api.module.Module;
 import me.lyric.infinity.api.setting.Setting;
+import me.lyric.infinity.api.util.string.ChatFormat;
 import me.lyric.infinity.mixin.mixins.accessors.ISPacketChat;
 import net.minecraft.network.play.server.SPacketChat;
 import net.minecraft.util.text.TextComponentString;
@@ -13,13 +13,15 @@ import net.minecraft.util.text.TextComponentString;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 /**
-    @author lyric - this is kinda broken
+    @author lyric
  */
 
 public class BetterChat
         extends Module {
     public final Setting<Boolean> timeStamps = register(new Setting<>("Timestamps","Does what it says on the tin lad",  true));
     public final Setting<Boolean> giantBeetleSoundsLikeJackhammer = register(new Setting<>("NoRect","Removes rectangle", true));
+    public Setting<ChatFormat.Color> bracketColor = register(new Setting<>("BracketColor","Colour of the brackets.", ChatFormat.Color.DARK_PURPLE));
+    public Setting<ChatFormat.Color> commandColor = register(new Setting<>("NameColor","Colour of timestamps", ChatFormat.Color.LIGHT_PURPLE));
     private static BetterChat INSTANCE = new BetterChat();
 
     public BetterChat() {
@@ -48,6 +50,8 @@ public class BetterChat
         String date = new SimpleDateFormat("k:mm").format(new Date());
         final String bracket = "<";
         final String bracket2 = ">";
-        return ChatFormatting.DARK_PURPLE + bracket +  ChatFormatting.LIGHT_PURPLE + date + ChatFormatting.DARK_PURPLE + bracket2 + " " + ChatFormatting.RESET;
+        return ChatFormat.coloredString(bracket, this.bracketColor.getValue()) + ChatFormat.coloredString("Infinity", this.commandColor.getValue()) + ChatFormat.coloredString(bracket2, this.bracketColor.getValue());
     }
+
 }
+
