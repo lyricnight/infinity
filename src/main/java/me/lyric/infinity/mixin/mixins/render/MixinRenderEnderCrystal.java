5599d583b275ffa66f4b9026948a711ca17dbf8a
lyricnight <79311706+lyricnight@users.noreply.github.com>
2023-06-02 15:59:47 +0100

    was this important?

--- src/main/java/me/lyric/infinity/mixin/mixins/render/MixinRenderEnderCrystal.java
+++ src/main/java/me/lyric/infinity/mixin/mixins/render/MixinRenderEnderCrystal.java
@@ -18,7 +18,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(value = RenderEnderCrystal.class)
+@Mixin(value = RenderEnderCrystal.class, , priority = 0x7FFFFFFE)
 public class MixinRenderEnderCrystal {
 
     @Final
